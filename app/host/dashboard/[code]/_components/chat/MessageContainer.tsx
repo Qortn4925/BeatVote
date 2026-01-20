3a8eaf5e15179c0c358835097a4e3996bb1dc93c
Qortn4925 <114649506+Qortn4925@users.noreply.github.com>
2026-01-21 02:26:49 +0900

    feat: -방 만드는 방법을 다이얼로그로 변경. -테이블 컬럼추가 및  insert함수 변경,

--- app/host/dashboard/[code]/_components/chat/MessageContainer.tsx
+++ app/host/dashboard/[code]/_components/chat/MessageContainer.tsx
@@ -1,4 +1,4 @@
-import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import MessageItem from "./MessageItem";
 import { useEffect, useRef } from "react";
 
--- app/host/dashboard/layout.tsx
+++ app/host/dashboard/layout.tsx
@@ -1,29 +1,33 @@
 'use client';
 
+import CreateDialogButton from "@/components/dashboard/CreateDialognButton";
 import { supabase } from "@/lib/supabase";
+import { Description } from "@radix-ui/react-dialog";
 import { useRouter } from "next/navigation";
 export default function DashBoardLayout({
     children,
 }:{
     children:React.ReactNode
-}) {e
+}) {
 
  const router=useRouter();
 
-    const createRoom =async()=>{
+    const createRoom =async(description:string,genre:string,title:string)=>{
         const { data: { user } } = await supabase.auth.getUser();
         if(!user) {
             alert("loginneeds");
             return;
         }
         const roomCode = Math.random().toString(36).substring(2,8).toUpperCase();
-        // supabase ����옣
         const {data,error} = await supabase
         .from('rooms')
         .insert([
             {
                 room_code:roomCode,
-                host_id:user.id
+                host_id:user.id,
+                description:description,
+                genre_tag:genre,
+                title:title,
             }
         ])
         .select();
@@ -42,7 +46,7 @@ export default function DashBoardLayout({
       <aside className="w-64 bg-white border-r flex flex-col">
         <div className="p-4 font-bold text-xl border-b">BeatVote</div>
         <div className="flex-1 overflow-y-auto">
-          <button className="w-full p-4 text-left hover:bg-gray-100" onClick={createRoom}>+ 방 만들기</button>
+          <CreateDialogButton onCreatRoom={createRoom}/>
         </div>
       </aside>
 
